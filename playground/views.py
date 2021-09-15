b0500abb01c46e09c9dd8aed0a9f9afd37e05f77
Jerud-Moyer <jerudmoyer@gmail.com>
2021-09-15 15:23:11 -0700

    doin work

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,27 @@
+from django.db.models.aggregates import Count
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.db.models import Q, F, Value
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.contenttypes.models import ContentType
+from store.models import Product, Customer, OrderItem, Order
+from tags.models import TaggedItem
 
 
-def calculate():
-    x = 1
-    y = 2
-    return x
 
 
 def say_hello(request):
-    x = calculate()
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    # queryset = Product.objects.filter(id__in=OrderItem.objects.values('product_id').distinct()).order_by('title')
+    # queryset = Order.objects.aggregate(count=Count('id'))
+
+    content_type = ContentType.objects.get_for_model(Product)
+
+    TaggedItem.objects. \
+        select_related('tag') \
+        .filter(
+            content_type=content_type,
+            object_id=1
+        )
+
+
+
+    return render(request, 'hello.html', {'name': 'Jerud'})
